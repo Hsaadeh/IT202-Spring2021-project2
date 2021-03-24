700ee1689c8e2014f2656ecc62ae298f642a162d
Hsaadeh <hsaadeh622@gmail.com>
2021-03-24 15:34:55 -0500

    menu and start button
    used paint 3d to piece together a menu image. Then I used a pixel python program to make a pixel menu background. The menu is complete for now. The start button functionality was started.

---
+++ app.js
@@ -0,0 +1,10 @@
+// javascript functionality for the site
+
+
+// start button
+startButton = document.querySelector("#startGame");
+startButton.addEventListener("click", (event) => {
+    // dont display the menu
+    // display the game screen
+    console.log("start");
+})
