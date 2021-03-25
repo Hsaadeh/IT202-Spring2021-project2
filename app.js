f15e0e17c394a512dfb62713e3611142d0a029f8
Hsaadeh <hsaadeh622@gmail.com>
2021-03-25 17:00:35 -0500

    gameCanvas

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 // javascript functionality for the site
 
+// global variables
+menu = document.querySelector("#menu");
+gameCanvas = document.querySelector("#gameCanvas");
 
 // start button
 startButton = document.querySelector("#startGame");
 startButton.addEventListener("click", (event) => {
     // dont display the menu
+    menu.style.display = "none";
     // display the game screen
+    gameCanvas.style.display = "block";
     console.log("start");
 })
